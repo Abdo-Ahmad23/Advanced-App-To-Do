7d600cf502f0278c67274145d6a90984cbb40374
Abdo-Ahmad23 <thisforwork23@gmail.com>
2024-09-17 17:07:24 +0300

    version3 for to_do_app

---
+++ advanced_to_do_app/models/task.py
@@ -0,0 +1,48 @@
+from odoo import fields, models,api
+
+class Task(models.Model):
+    _name='task'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
+
+
+    name=fields.Char(string='Task Name')
+    assign_to=fields.Many2one('employee',string='Assign To',tracking=1)
+    description=fields.Text(string='Description')
+    due_date=fields.Date(string='Due Date',tracking=1)
+    active=fields.Boolean(default=True)
+    state=fields.Selection([
+        ('new','New'),
+        ('in_progress','In Progress'),
+        ('completed','Completed')
+    ],default='new')
+    project_id = fields.Many2one('project', string='Project')
+    subtask_ids = fields.One2many('subtask', 'task_id', string='Subtasks')
+    # active=fields.Boolean()
+    def action_new(self):
+        for rec in self:
+            rec.state='new'
+    
+    def action_in_progress(self):
+        for rec in self:
+            rec.state='in_progress'
+
+    def action_completed(self):
+        for rec in self: 
+            rec.state='completed'
+
+
+
+class SubTask(models.Model):
+    _name = 'subtask'
+    _description = 'Subtask'
+
+    name = fields.Char(string='SubTask Name')
+    description = fields.Text(string='Description')
+    state = fields.Selection([
+        ('new', 'New'),
+        ('in_progress', 'In Progress'),
+        ('completed', 'Completed')
+    ], default='new')
+    task_id = fields.Many2one('task' )
+
+
